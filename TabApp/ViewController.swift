6d880c3c1e4c2ca6c4f4a960c29cc2179e507a04
Neilan Sankpal <sankp001@rangers.uwp.edu>
2019-04-02 18:18:04 -0500

    Initial Commit

---
+++ TabApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabApp
+//
+//  Created by Neilan Sankpal on 4/2/19.
+//  Copyright © 2019 Neilan Sankpal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TabAppTests/TabAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TabAppTests.swift
+//  TabAppTests
+//
+//  Created by Neilan Sankpal on 4/2/19.
+//  Copyright © 2019 Neilan Sankpal. All rights reserved.
+//
+
+import XCTest
+@testable import TabApp
+
+class TabAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
